75ba2398dc8b39fd4a732bfabec90d423e2f86db
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-01-22 23:40:24 +0530

    [Add] UI: Hero Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import Navbar from './components/ui/Navbar';
+import Navbar from './components/Navbar';
 import Form from './components/Form';
 import Preview from './components/Preview';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
       <div>
         <Form />
         <Preview />
+        
       </div>
 
     </>
